1d094623ee66a2cbc1e00fe22412a9c043314da7
Siddharth Seth <140103717+sidharthhhhhh@users.noreply.github.com>
2023-08-09 02:11:38 +0530

    Added solution - LeetHub

---
+++ Next element with greater frequency - GFG/next-element-with-greater-frequency.java
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class GFG
+{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        // int arr[] = new int[10000];
+        
+        int t = sc.nextInt();
+        
+        while(t-->0)
+        {
+            int n = sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0; i<n; i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            
+            solver x = new solver();
+            int res[]=x.print_next_greater_freq(arr,n);
+            StringBuffer sb=new StringBuffer("");
+            for(int i : res){
+                sb.append(i+" ");
+            }
+            System.out.println(sb);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class solver
+{
+    static int[] print_next_greater_freq(int arr[], int n)
+    {
+        int[] freq = new int[n];
+        int[] ans = new int[n];
+        
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        Stack<Integer> stc = new Stack<>();
+        
+        
+        for(int i = 0;i<n;i++){
+            if(hm.containsKey(arr[i])){
+               hm.put(arr[i],hm.get(arr[i]) + 1); 
+            }
+            else{
+                hm.put(arr[i],1);
+            }
+        }
+        for(int i = 0;i<n;i++){
+            freq[i] = hm.get(arr[i]);
+        }
+        ans[n-1] = -1;
+        st.push(freq[n-1]);
+        stc.push(arr[n-1]);
+        // Set<Integer> keys = hm.keySet();
+        // System.out.println(hm);
+        // System.out.println(freq);
+        
+        for(int i = n-2;i>=0;i--){
+            
+                while(st.size()>0 && freq[i] >= st.peek()){
+                    st.pop();
+                    stc.pop();
+                }
+                if(st.size() == 0){
+                    ans[i] = -1;
+                }
+                else{
+                    ans[i] = stc.peek();
+                    
+                    // int t = st.peek();
+                    // for(int k : keys){
+                    //     if(hm.get(k) == t){
+                    //         ans[i] = k;
+                    //     }
+                    // }
+                    
+                }
+                st.push(freq[i]);
+                stc.push(arr[i]);
+            
+        }
+        return ans;
+        
+        
+        
+    }
+}
+
