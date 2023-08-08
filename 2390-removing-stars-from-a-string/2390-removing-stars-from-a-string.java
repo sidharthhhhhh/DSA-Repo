218c507fcc14fd5eb1f104fa38f2ad1fe621f32c
Siddharth Seth <140103717+sidharthhhhhh@users.noreply.github.com>
2023-08-08 22:24:18 +0530

    Time: 33 ms (91.58%), Space: 44.3 MB (97.18%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,40 +1,40 @@
 class Solution {
     public String removeStars(String s) {
         char c = '*';
-        // int in = -1;
-//         StringBuilder ans = new StringBuilder();
+        int in = -1;
+        StringBuilder ans = new StringBuilder();
       
 
-//         if(s.length() == 0){
-//             return ans.toString();
-//         }
+        if(s.length() == 0){
+            return ans.toString();
+        }
 
-//         for(int i = 0;i<s.length();i++){
-//             if(s.charAt(i) == c){
-//                 ans.deleteCharAt(in);
-//                 in--;
-
-//             }
-//             else{
-//                 ans.append(s.charAt(i));
-//                 in++;
-//             }
-//         }
-//         return ans.toString();
-        Stack<Character> st = new Stack<>();
         for(int i = 0;i<s.length();i++){
             if(s.charAt(i) == c){
-                st.pop();
+                ans.deleteCharAt(in);
+                in--;
+
             }
             else{
-                st.push(s.charAt(i));
+                ans.append(s.charAt(i));
+                in++;
             }
         }
-        String ans = "";
-        while(st.size() > 0){
-            ans = st.pop() + ans;
-        }
-        return ans;
+        return ans.toString();
+        // Stack<Character> st = new Stack<>();
+        // for(int i = 0;i<s.length();i++){
+        //     if(s.charAt(i) == c){
+        //         st.pop();
+        //     }
+        //     else{
+        //         st.push(s.charAt(i));
+        //     }
+        // }
+        // String ans = "";
+        // while(st.size() > 0){
+        //     ans = st.pop() + ans;
+        // }
+        // return ans;
 
     }
 }
