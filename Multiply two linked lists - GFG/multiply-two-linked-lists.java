0aa9188b8fc8514c3a0cf89366b1558adffd0a06
Siddharth Seth <140103717+sidharthhhhhh@users.noreply.github.com>
2023-08-29 11:28:55 +0530

    Added solution - LeetHub

---
+++ Multiply two linked lists - GFG/multiply-two-linked-lists.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        next=null;
+    }
+}
+
+class MultiplyTwoLL{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			Node head=null;
+			Node phead=null;
+			int n=sc.nextInt();
+			while(n-->0){
+				int n1=sc.nextInt();
+				if(head==null)
+				head=new Node(n1);
+			else{
+				addHead(head,n1);
+			}
+			}
+			int m=sc.nextInt();
+			while(m-->0){
+				int n1=sc.nextInt();
+				if(phead==null)
+				phead=new Node(n1);
+			else{
+				addPhead(phead,n1);
+			}
+			}
+		GfG g=new GfG();
+
+System.out.println(g.multiplyTwoLists(head,phead));
+		}
+	}
+    public static void addHead(Node node,int a) {
+          if (node == null)
+            return;
+		if(node.next==null)
+			node.next=new Node(a);
+		else
+			addHead(node.next,a);
+	}
+	public static void addPhead(Node node,int a){
+		 if (node == null)
+            return;
+		if(node.next==null)
+		node.next=new Node(a);
+	else
+		addPhead(node.next,a);
+	}
+	}
+	
+	
+// } Driver Code Ends
+
+
+/*Node is defined as
+class Node
+{
+    int data; 
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class GfG{
+  /*You are required to complete this method */
+   public long multiplyTwoLists(Node l1,Node l2){
+          //add code here.
+          long mod = 1000000007;
+          Node t1 = l1;
+          Node t2 = l2;
+          long n1 = 0;
+          long n2 = 0;
+          
+          while(t1 != null){
+              n1 = n1*10 + t1.data;
+              n1 = n1 % mod;
+              t1 = t1.next;
+          }
+          while(t2 != null){
+              n2 = n2*10 + t2.data;
+              n2 = n2 % mod;
+              t2 = t2.next;
+          }
+          
+          long ans = (n1 * n2)%mod;
+          return ans;
+   }
+}
