eb56b2421592963b6996174fc42592b8ef5a5f63
Siddharth Seth <140103717+sidharthhhhhh@users.noreply.github.com>
2023-10-01 16:04:24 +0530

    Added solution - LeetHub

---
+++ Boundary traversal of matrix - GFG/boundary-traversal-of-matrix.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            String s[] = in.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int a[][] = new int[n][m];
+            s = in.readLine().trim().split(" ");
+            int ind=0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = Integer.parseInt(s[ind++]);
+                }
+            }
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.boundaryTraversal(a, n, m);
+            for (int i : ans) {
+                out.print(i + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to return list of integers that form the bounda
+    //traversal of the matrix in a clockwise manner.
+    // static ArrayList<Integer> Traverse(ArrayList<Integer>arr,int r,int c){
+        
+    //     for(int )
+        
+    // }
+    static ArrayList<Integer> boundaryTraversal(int matrix[][], int n, int m)
+    {
+        // code here 
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        if(m == 1){
+            for(int i = 0;i<n;i++){
+                arr.add(matrix[i][0]);
+            }
+            return arr;
+        }
+        if(n == 1){
+            for(int i = 0;i<m;i++){
+                arr.add(matrix[0][i]);
+            }
+            return arr;
+        }
+        
+        for(int i = 0;i<m;i++){
+            arr.add(matrix[0][i]);
+        }
+        for(int i = 1;i<n;i++){
+            arr.add(matrix[i][m-1]);
+        }
+        for(int i = m-2;i>=0;i--){
+            arr.add(matrix[n-1][i]);
+        }
+        for(int i = n-2;i>0;i--){
+            arr.add(matrix[i][0]);
+        }
+        
+        return arr;
+    }
+}
