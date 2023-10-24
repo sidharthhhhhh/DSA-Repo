46ed42a402fc17017cca7807ab878d3890c8ea56
Siddharth Seth <140103717+sidharthhhhhh@users.noreply.github.com>
2023-10-25 03:05:57 +0530

    Added solution - LeetHub

---
+++ Rotate a 2D array without using extra space - GFG/rotate-a-2d-array-without-using-extra-space.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * n + j]);
+                }
+            }
+
+            new Solution().rotateMatrix(arr, n);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    out.print(arr[i][j] + " ");
+                }
+            }
+            out.println();
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    void rotateMatrix(int arr[][], int n) {
+        // code here
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        
+        for(int i = 0;i<n;i++){
+            int u = 0;
+            int d = n - 1;
+            
+            while(u<d){
+                int temp = arr[u][i];
+                arr[u][i] = arr[d][i];
+                arr[d][i] = temp;
+                u++;
+                d--;
+                
+            }
+        }
+    }
+}
